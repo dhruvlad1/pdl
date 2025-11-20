740599764b3ea05976bf3edb8fc13c35078e1fb9
Dhruv Lad <laddhruv495@gmail.com>
2025-11-20 10:05:58 +0530

    Update disc.cpp

--- os/disc.cpp
+++ os/disc.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/* #include <bits/stdc++.h>
 using namespace std;
 #define CYLINDERS 5000
 #define REQUESTS 1000
@@ -127,3 +127,193 @@ int main(int argc, char* argv[]) {
     cout << "C-LOOK: " << CLOOK(requests, head) << endl;
     return 0;
 }
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- FCFS ----------
+int FCFS(vector<int> requests, int head) {
+    int total = 0;
+    for (int r : requests) {
+        total += abs(r - head);
+        head = r;
+    }
+    return total;
+}
+
+// ---------- SSTF ----------
+int SSTF(vector<int> requests, int head) {
+    int total = 0;
+    vector<bool> visited(requests.size(), false);
+
+    for (int i = 0; i < requests.size(); i++) {
+        int idx = -1, dist = INT_MAX;
+        for (int j = 0; j < requests.size(); j++) {
+            if (!visited[j] && abs(requests[j] - head) < dist) {
+                dist = abs(requests[j] - head);
+                idx = j;
+            }
+        }
+        visited[idx] = true;
+        total += abs(requests[idx] - head);
+        head = requests[idx];
+    }
+    return total;
+}
+
+// ---------- SCAN ----------
+int SCAN(vector<int> requests, int head, int disk_size) {
+    vector<int> left, right;
+    for (int r : requests) {
+        if (r < head) left.push_back(r);
+        else right.push_back(r);
+    }
+
+    left.push_back(0);
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+
+    int total = 0;
+    int cur = head;
+
+    for (int r : right) {
+        total += abs(cur - r);
+        cur = r;
+    }
+
+    total += abs(cur - left.back());
+    cur = left.back();
+
+    for (int i = left.size() - 2; i >= 0; i--) {
+        total += abs(cur - left[i]);
+        cur = left[i];
+    }
+
+    return total;
+}
+
+// ---------- C-SCAN ----------
+int CSCAN(vector<int> requests, int head, int disk_size) {
+    vector<int> left, right;
+    for (int r : requests) {
+        if (r < head) left.push_back(r);
+        else right.push_back(r);
+    }
+
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+
+    int total = 0;
+    int cur = head;
+
+    for (int r : right) {
+        total += abs(cur - r);
+        cur = r;
+    }
+
+    total += abs(cur - (disk_size - 1));
+    total += (disk_size - 1);
+
+    cur = 0;
+
+    for (int r : left) {
+        total += abs(cur - r);
+        cur = r;
+    }
+
+    return total;
+}
+
+// ---------- LOOK ----------
+int LOOK(vector<int> requests, int head) {
+    vector<int> left, right;
+
+    for (int r : requests) {
+        if (r < head) left.push_back(r);
+        else right.push_back(r);
+    }
+
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+
+    int total = 0;
+    int cur = head;
+
+    for (int r : right) {
+        total += abs(cur - r);
+        cur = r;
+    }
+
+    for (int i = left.size() - 1; i >= 0; i--) {
+        total += abs(cur - left[i]);
+        cur = left[i];
+    }
+
+    return total;
+}
+
+// ---------- C-LOOK ----------
+int CLOOK(vector<int> requests, int head) {
+    vector<int> left, right;
+
+    for (int r : requests) {
+        if (r < head) left.push_back(r);
+        else right.push_back(r);
+    }
+
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+
+    int total = 0;
+    int cur = head;
+
+    for (int r : right) {
+        total += abs(cur - r);
+        cur = r;
+    }
+
+    if (!left.empty()) {
+        total += abs(cur - left.front());
+        cur = left.front();
+
+        for (int r : left) {
+            total += abs(cur - r);
+            cur = r;
+        }
+    }
+
+    return total;
+}
+
+// ---------- MAIN ----------
+int main() {
+    int disk_size = 5000;
+    int num_requests = 1000;
+    int head;
+
+    cout << "Enter initial head position: ";
+    cin >> head;
+
+    srand(time(0));
+    vector<int> requests(num_requests);
+    for (int i = 0; i < num_requests; i++)
+        requests[i] = rand() % disk_size;
+
+    cout << "\nGenerated " << num_requests << " random requests (0-" 
+         << disk_size - 1 << ")\n";
+    cout << "Initial Head Position: " << head << "\n";
+
+    cout << "\nTotal Head Movements:\n";
+    cout << "FCFS     : " << FCFS(requests, head) << "\n";
+    cout << "SSTF     : " << SSTF(requests, head) << "\n";
+    cout << "SCAN     : " << SCAN(requests, head, disk_size) << "\n";
+    cout << "C-SCAN   : " << CSCAN(requests, head, disk_size) << "\n";
+    cout << "LOOK     : " << LOOK(requests, head) << "\n";
+    cout << "C-LOOK   : " << CLOOK(requests, head) << "\n";
+
+    return 0;
+}
+
+
+
